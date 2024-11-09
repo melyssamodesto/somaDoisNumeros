040abbeb6da6466dc914dd0626ee312c2044c04c
Melyssa Modesto <melyssamodestodev@gmail.com>
2024-11-09 13:55:38 -0300

    primeiro commit

---
+++ somaDoisNumeros.py
@@ -0,0 +1,11 @@
+print("Digite o primeiro número:")
+primeiroNumero = input()
+
+print("Digite o segundo número:")
+segundoNumero = input()
+
+print("O primeiro número digitado foi:", primeiroNumero)
+print("O segundo número digitado foi:", segundoNumero)
+
+resultado = int(primeiroNumero) + int(segundoNumero)
+print("O resultado da soma foi:", resultado)
